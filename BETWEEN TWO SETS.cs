d030689fb741ab8b027d08429420fd588778b83f
tatojastine-n <jajanicoletato@gmail.com>
2025-10-02 22:12:08 +0800

    BETWEEN TWO SETS.cs

--- BETWEEN TWO SETS.cs
+++ BETWEEN TWO SETS.cs
@@ -1,105 +1,92 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'getTotalX' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
-int getTotalX(vector<int> a, vector<int> b) {
-
-}
-
-int main()
+class Result
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
 
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
 
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    int n = stoi(first_multiple_input[0]);
-
-    int m = stoi(first_multiple_input[1]);
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+          if (a.Count == 0 || b.Count == 0) return 0;
+        int maxA = a.Max();
+        int minB = b.Min();
+        if (maxA > minB) return 0;
+        int count = 0;
+        for (int x = maxA; x <= minB; x++)
+        {
+            bool isMultipleOfA = true;
+            foreach (int aa in a)
+            {
+                if (x % aa != 0)
+                {
+                    isMultipleOfA = false;
+                    break;
+                }
+            }
+            if (!isMultipleOfA) continue;
+            bool dividesAllB = true;
+            foreach (int bb in b)
+            {
+                if (bb % x != 0)
+                {
+                    dividesAllB = false;
+                    break;
+                }
+            }
+            if (dividesAllB)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 
-    string brr_temp_temp;
-    getline(cin, brr_temp_temp);
+}
 
-    vector<string> brr_temp = split(rtrim(brr_temp_temp));
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-    vector<int> brr(m);
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-    for (int i = 0; i < m; i++) {
-        int brr_item = stoi(brr_temp[i]);
+        int n = Convert.ToInt32(firstMultipleInput[0]);
 
-        brr[i] = brr_item;
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        textWriter.WriteLine(total);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
-
-    int total = getTotalX(arr, brr);
-
-    fout << total << "\n";
-
-    fout.close();
-
-    return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
 
     tokens.push_back(str.substr(start));
 
